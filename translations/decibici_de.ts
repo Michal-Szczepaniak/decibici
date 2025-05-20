40635e484e631a4d1274e1cf3799fc1b442700fc
pradomyc <pradomyc@iavgroup.local>
2025-05-20 11:54:14 +0200

    Adding translations for DE and UA

---
+++ translations/decibici_de.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation type="Über"></translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak.</source>
+        <translation type="Erstellt von Michał Szczepaniak."></translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation type="Lizenziert unter der GPLv3"></translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation type="Links"></translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation type="Spenden"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Decibici</source>
+        <translation type="Decibici"></translation>
+    </message>
+    <message>
+        <source>Device connected</source>
+        <translation type="Gerät verbunden"></translation>
+    </message>
+    <message>
+        <source>Device found</source>
+        <translation type="Gerät gefunden"></translation>
+    </message>
+    <message>
+        <source>Device not paired</source>
+        <translation type="Gerät nicht gekoppelt"></translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Device connected</source>
+        <translation type="Gerät verbunden"></translation>
+    </message>
+    <message>
+        <source>Device found</source>
+        <translation type="Gerät gefunden"></translation>
+    </message>
+    <message>
+        <source>Device not paired</source>
+        <translation type="Gerät nicht gekoppelt"></translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation type="Einschalten"></translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation type="Ausschalten"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="Über"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/decibici_ua.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation type="Про програму"></translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak.</source>
+        <translation type="Створено Міхалом Щепаняком."></translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation type="Ліцензія GPLv3"></translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation type="Посилання"></translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation type="Пожертвувати"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Decibici</source>
+        <translation type="Decibici"></translation>
+    </message>
+    <message>
+        <source>Device connected</source>
+        <translation type="Пристрій підключено"></translation>
+    </message>
+    <message>
+        <source>Device found</source>
+        <translation type="Пристрій знайдено"></translation>
+    </message>
+    <message>
+        <source>Device not paired</source>
+        <translation type="Пристрій не спарений"></translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Device connected</source>
+        <translation type="Пристрій підключено"></translation>
+    </message>
+    <message>
+        <source>Device found</source>
+        <translation type="Пристрій знайдено"></translation>
+    </message>
+    <message>
+        <source>Device not paired</source>
+        <translation type="Пристрій не спарений"></translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation type="Увімкнути"></translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation type="Вимкнути"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="Про програму"></translation>
+    </message>
+</context>
+</TS>
